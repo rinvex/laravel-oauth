05c2729270fce27914c8379df26c742bc1feb5e2
Abdelrahman Omran <me@omranic.com>
2021-02-11 03:50:15 +0400

    Remove testing features `actingAsClient` and `actingAs`

--- src/OAuth.php
+++ src/OAuth.php
@@ -61,66 +61,6 @@ class OAuth
     }
 
     /**
-     * Set the current user for the application with the given scopes.
-     *
-     * @param \Illuminate\Contracts\Auth\Authenticatable|\Rinvex\OAuth\Traits\HasApiTokens $user
-     * @param array                                                                        $scopes
-     * @param string                                                                       $guard
-     *
-     * @return \Illuminate\Contracts\Auth\Authenticatable
-     */
-    public static function actingAs($user, $scopes = [], $guard = 'api')
-    {
-        $accessToken = Mockery::mock(config('rinvex.oauth.models.access_token'))->shouldIgnoreMissing(false);
-
-        foreach ($scopes as $scope) {
-            $accessToken->shouldReceive('can')->with($scope)->andReturn(true);
-        }
-
-        $user->withAccessToken($accessToken);
-
-        if (isset($user->wasRecentlyCreated) && $user->wasRecentlyCreated) {
-            $user->wasRecentlyCreated = false;
-        }
-
-        app('auth')->guard($guard)->setUser($user);
-
-        app('auth')->shouldUse($guard);
-
-        return $user;
-    }
-
-    /**
-     * Set the current client for the application with the given scopes.
-     *
-     * @param \Rinvex\OAuth\Models\Client $client
-     * @param array                       $scopes
-     *
-     * @return \Rinvex\OAuth\Models\Client
-     */
-    public static function actingAsClient($client, $scopes = [])
-    {
-        $accessToken = app('rinvex.oauth.access_token');
-
-        $accessToken->client_id = $client->id;
-        $accessToken->setRelation('client', $client);
-
-        $accessToken->scopes = $scopes;
-
-        $mock = Mockery::mock(ResourceServer::class);
-        $mock->shouldReceive('validateAuthenticatedRequest')
-            ->andReturnUsing(function (ServerRequestInterface $request) use ($accessToken) {
-                return $request->withAttribute('oauth_client_id', $accessToken->client->id)
-                    ->withAttribute('oauth_access_token_id', $accessToken->id)
-                    ->withAttribute('oauth_scopes', $accessToken->scopes);
-            });
-
-        app()->instance(ResourceServer::class, $mock);
-
-        return $client;
-    }
-
-    /**
      * The location of the encryption keys.
      *
      * @param string $file
