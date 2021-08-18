7cacd60d4ed47f8ac701cc7cf431fd7cef035c51
Abdelrahman Omran <me@omranic.com>
2021-08-18 05:29:25 +0400

    Fix crypt key config issue

--- src/Providers/OAuthServiceProvider.php
+++ src/Providers/OAuthServiceProvider.php
@@ -261,7 +261,7 @@ class OAuthServiceProvider extends ServiceProvider
      */
     protected function makeCryptKey($type)
     {
-        $key = str_replace('\\n', "\n", config("rinvex.oauth.{$type}_key"));
+        $key = str_replace('\\n', "\n", (string) config("rinvex.oauth.{$type}_key"));
 
         if (! $key) {
             $key = 'file://'.KeysCommand::keyPath('oauth-'.$type.'.key');
